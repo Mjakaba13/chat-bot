e9a316238e03c5b3a212a6d10b52f4e3cf776949
Mjakaba13 <malcolmakaba@gmail.com>
2022-06-12 21:43:47 +0000

    First

---
+++ index.js
@@ -0,0 +1,20 @@
+let sector = document.getElementById("sector");
+let searchInput = document.getElementById("sear");
+let messageInput = document.getElementById("message-input");
+// let messageValue = messageInput.value;
+let sendButton = document.getElementById("send");
+let textbody = document.getElementById("textbody");
+
+sendButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  textbody.innerHTML += `
+  <p id="value">${document.getElementById("message-input").value}</p>
+  `;
+  if (document.getElementById("message-input").value.includes("Hi")) {
+    textbody.innerHTML += `
+      <p id="reply">Hi Madam, Welcome back.</p>
+      `;
+  }
+  validator();
+  document.getElementById("message-input").value = null;
+});
